959b30e54dade720011a4dd66c714c10384f10a1
willian <williannlins@gmail.com>
2024-04-07 17:47:00 -0300

    ok

--- src/components/ToDo/index.tsx
+++ src/components/ToDo/index.tsx
@@ -7,9 +7,9 @@ export function ToDo() {
     <div className={styles.contentListTodo}>
       <div>
         <input name='todo' type='checkbox' id='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.' />
-        <label htmlFor='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.' ></label>
+        <label htmlFor='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates cum id deleniti nobis molestiae dignissimos dolores! Deserunt earum inventore dolores quam consequuntur labore, unde alias similique veniam, molestiae exercitationem illum.</label>
       </div>
-      <label  htmlFor='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.</label>
+      {/* :<label className={styles.teste}  htmlFor='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.' >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui blanditiis quisquam sequi illum sint aliquid totam tenetur, sit numquam quam amet similique, asperiores adipisci, harum eius iusto accusamus fugit vel.</label> */}
       <Trash size={20} />
     </div>
   )
